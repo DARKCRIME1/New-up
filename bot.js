6d83ac124665bf8294bba8ba669b411e22cf381a
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-03-04 08:42:08 +0530

    Create bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,9 @@ const path = require("path");
 const events = require("./events");
 const chalk = require('chalk');
 const config = require('./config');
-const {makeWASocket, WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
+const makeWASocket, { AnyMessageContent, delay, DisconnectReason, fetchLatestBaileysVersion, makeInMemoryStore, useSingleFileAuthState } require ('@adiwajshing/baileys')
+const { Boom } require ('@hapi/boom')
+const P require ('pino')
 const {Message, StringSession, Image, Video} = require('./Trex/');
 const { DataTypes } = require('sequelize');
 const { getMessage } = require("./plugins/sql/greetings");
@@ -57,31 +59,92 @@ Array.prototype.remove = function() {
     return this;
 };
 
-async function Trex () {
-    const sock = makeWASocket({
-        // can provide additional config here
-        config.SESSION: true
-    })
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update
-        if(connection === 'close') {
-            const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
-            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
-            // reconnect if not logged out
-            if(shouldReconnect) {
-                sock = startSock()
-            }
-        } else if(connection === 'open') {
-            console.log('opened connection')
-        }
-    })
-    sock.ev.on('messages.upsert', m => {
-        console.log(JSON.stringify(m, undefined, 2))
+// the store maintains the data of the WA connection in memory
+// can be written out to a file & read from it
+const store = makeInMemoryStore({ logger: P().child({ level: 'debug', stream: 'store' }) })
+store.readFromFile('./baileys_store_multi.json')
+// save every 10s
+setInterval(() => {
+	store.writeToFile('./baileys_store_multi.json')
+}, 10_000)
 
-        console.log('replying to', m.messages[0].key.remoteJid)
-        await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Hello there!' })
-    })
+const { state, saveState } = useSingleFileAuthState('./auth_info_multi.json')
+
+// start a connection
+const startSock = async() => {
+	// fetch latest version of WA Web
+	const { version, isLatest } = await fetchLatestBaileysVersion()
+	console.log(`using WA v${version.join('.')}, isLatest: ${isLatest}`)
+
+	const sock = makeWASocket({
+		version,
+		logger: P({ level: 'trace' }),
+		printQRInTerminal: true,
+		auth: state,
+		// implement to handle retries
+		getMessage: async key => {
+			return {
+				conversation: 'hello'
+			}
+		}
+	})
+
+	store.bind(sock.ev)
+
+	const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
+		await sock.presenceSubscribe(jid)
+		await delay(500)
+
+		await sock.sendPresenceUpdate('composing', jid)
+		await delay(2000)
+
+		await sock.sendPresenceUpdate('paused', jid)
+
+		await sock.sendMessage(jid, msg)
+	}
+    
+	sock.ev.on('chats.set', item => console.log(`recv ${item.chats.length} chats (is latest: ${item.isLatest})`))
+	sock.ev.on('messages.set', item => console.log(`recv ${item.messages.length} messages (is latest: ${item.isLatest})`))
+	sock.ev.on('contacts.set', item => console.log(`recv ${item.contacts.length} contacts`))
+
+	sock.ev.on('messages.upsert', async m => {
+		console.log(JSON.stringify(m, undefined, 2))
+        
+		const msg = m.messages[0]
+		if(!msg.key.fromMe && m.type === 'notify') {
+			console.log('replying to', m.messages[0].key.remoteJid)
+			await sock!.sendReadReceipt(msg.key.remoteJid, msg.key.participant, [msg.key.id])
+			await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid)
+		}
+        
+	})
+
+	sock.ev.on('messages.update', m => console.log(m))
+	sock.ev.on('message-receipt.update', m => console.log(m))
+	sock.ev.on('presence.update', m => console.log(m))
+	sock.ev.on('chats.update', m => console.log(m))
+	sock.ev.on('contacts.upsert', m => console.log(m))
+
+	sock.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if(connection === 'close') {
+			// reconnect if not logged out
+			if((lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+				startSock()
+			} else {
+				console.log('connection closed')
+			}
+		}
+        
+		console.log('connection update', update)
+	})
+	// listen for when the auth credentials is updated
+	sock.ev.on('creds.update', saveState)
+
+	return sock
 }
+
+startSock()
     
 // ════════════════════WA CONNECTION🍁🍁🍁
     
